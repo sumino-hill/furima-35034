235ef8dd736f0f1db82743aeee81b941b1a23d9f
sumino-hill <sirokichi.0823@gmail.com>
2021-05-19 11:44:49 +0900

    ログイン・ログアウト機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,14 +4,9 @@ class ApplicationController < ActionController::Base
 
   private
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name , :last_name])
-end
-
- 
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name , :last_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname , :last_name , :first_name , :last_name_kana , :first_name_kana , :birthday])
   end
+
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|

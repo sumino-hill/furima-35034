7a73b7ccf4a1fb707db9d1ef8cb06e917a8a45e5
sumino-hill <sirokichi.0823@gmail.com>
2021-06-03 00:11:25 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,4 +5,9 @@ class ItemsController < ApplicationController
 
   def new
   end
+  private
+
+  def item_params
+    params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,5 @@
 class Item < ApplicationRecord
+  has_one_attached :image
   with_options presence: true do
     validates :name
     validates :explain

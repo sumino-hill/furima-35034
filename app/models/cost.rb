e6f4974ed6a932ab41ebbc3e24824d5a39a36f1e
sumino-hill <sirokichi.0823@gmail.com>
2021-06-03 17:33:28 +0900

    Active Storageの導入2

---
+++ app/models/cost.rb
@@ -0,0 +1,2 @@
+class  < ActiveHash::Base
+end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,7 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
+  belongs_to :user
   has_one_attached :image
   with_options presence: true do
     validates :name

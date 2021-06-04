1df405af2d718f6db625ea91eddbfd6f0c366e10
sumino-hill <sirokichi.0823@gmail.com>
2021-06-04 14:22:32 +0900

    プルダウン完成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,10 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :prefecture
+  belongs_to :category
+  belongs_to :cost
+  belongs_to :shipping_day
+  belongs_to :state
   belongs_to :user
   has_one_attached :image
   with_options presence: true do

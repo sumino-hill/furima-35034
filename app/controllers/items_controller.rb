87248b77b124b88e01ed615ca8035e095575e146
sumino-hill <sirokichi.0823@gmail.com>
2021-06-09 19:54:28 +0900

    削除機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:edit, :show, :update]
+  before_action :set_item, only: [:edit, :show, :update, :destroy]
   before_action :set_edit, only: [:edit, :update]
   def index
     @items = Item.all.order("created_at ASC")
@@ -32,6 +32,12 @@ class ItemsController < ApplicationController
       render :edit
     end
   end
+
+  def destroy
+    @item.destroy
+    redirect_to root_path
+  end
+
 
   private
 

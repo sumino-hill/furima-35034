144841bc88c47cc6466442c91b10ac8a32db7687
sumino-hill <sirokichi.0823@gmail.com>
2021-06-10 20:06:41 +0900

    addressテーブル保存

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -1,6 +1,6 @@
 class PurchaseAddress
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number, :user_id, :item_id
+  attr_accessor :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number, :user_id, :item_id, :purchase_id
   with_options presence: true do
     validates :postal_code
     validates :prefecture_id
@@ -9,9 +9,9 @@ class PurchaseAddress
     validates :phone_number
   end
   def save
-    purchase = Purchase.create(user_id: user_id, item_id: item_id)
+    purchase = Purchase.create!(user_id: user_id, item_id: item_id)
 
-    address = Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, 
-                             addresses: addresses, building: building, phone_number: phone_number)
+    address = Address.create!(postal_code: postal_code, prefecture_id: prefecture_id, city: city, 
+                             addresses: addresses, building: building, phone_number: phone_number, purchase_id: purchase.id)
   end
 end

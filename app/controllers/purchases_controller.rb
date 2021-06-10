57b8c7fff87503ec461e5f754f79720ce0bc91c0
sumino-hill <sirokichi.0823@gmail.com>
2021-06-10 19:34:23 +0900

    購入ページ完成

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -5,7 +5,7 @@ class PurchasesController < ApplicationController
   end
 
   def create
-    @item = Item.find(params[:item_id])
+     @item = Item.find(params[:item_id])
     @purchase_address = PurchaseAddress.new(purchase_params)
     if @purchase_address.valid?
       @purchase_address.save
